4687b1cc996818f7dfcc29562a6467a4af0b01a2
RyanMKrol <ryankrol.m@gmail.com>
2021-03-13 17:02:20 +0000

    Add basic method to fetch repo data

---
+++ src/modules/fetch/index.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch';
+
+/**
+ * method to fetch repositories for a given user
+ *
+ * @param {string} username the user to fetch data for
+ * @returns {Promise<JSON>} repositories for a user
+ */
+async function fetchRepos(username) {
+  return fetch(`https://api.github.com/users/${username}/repos`)
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+}
+
+export default fetchRepos;
