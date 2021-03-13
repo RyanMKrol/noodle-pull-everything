fe28ce83999489580cefcec55667a82dbe5a4e54
RyanMKrol <ryankrol.m@gmail.com>
2021-03-13 17:18:24 +0000

    Update index to tie all modules together

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import customFunction from './module';
+#!/usr/bin/env node
 
-export default customFunction;
+import ask from './modules/input';
+import fetch from './modules/fetch';
+import pull from './modules/pull';
+
+/**
+ * method to run the tool
+ */
+async function main() {
+  const data = await ask();
+  const repos = await fetch(data.username);
+
+  const urls = repos.map((repo) => repo.html_url);
+
+  pull(urls);
+}
+
+main();
